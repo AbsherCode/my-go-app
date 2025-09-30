fa4810930aecdc9badb3ba4e7f47db1cc3bef393
Marcus Absher <marcus.absher@gmail.com>
2025-09-30 12:34:57 -0700

    practiced variables and global and local scope

--- types/variables/main.go
+++ types/variables/main.go
@@ -3,6 +3,9 @@ package main
 
 import "fmt"
 
+// use type inference to create a package-level variable "val" and assign it a value of "global"
+var val = "global"
+
 // declare package-level variables of type int
 var d, e, f int
 
@@ -23,8 +26,37 @@ func main() {
 
 	// print the package-level variables x, y, and z
 	printVars()
+
+	// create a local variable "val" and assign it an int value
+	val := 42
+
+	// print the value of the local variable "val"
+	fmt.Printf("%T, local val = %v\n", val, val)
+
+	// print the value of the package-level variable "val"
+	printGlobal()
+
+	// update the package-level variable "val" and print it again
+	updateGlobal()
+	printGlobal()
+
+	// print the pointer address of the local variable "val"
+	fmt.Printf("%T, local &val = %v\n", &val, &val)
+
+	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+	*(&val) = 100
+	fmt.Println("local val = ", val)
 }
 
 func printVars() {
 	fmt.Println("x, y, z", x, y, z)
+}
+
+
+func updateGlobal() {
+	val = "updated global"
+}
+
+func printGlobal() {
+	fmt.Println("global val = ", val)
 }
